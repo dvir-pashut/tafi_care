b13bd235c738be2f491aa1c6137cb81020d55179
chkp-dvirpa <chkp-dvirpa@users.noreply.github.com>
2024-06-21 11:27:22 +0000

    deploy: a29ed48d42ada2e1f0bc16dbf0350ad169dfbb3b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1964239582"
+    serviceWorkerVersion: "380737473"
   }
 });
